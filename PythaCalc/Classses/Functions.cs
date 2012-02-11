0f7c187360f8d7df3417e034e4dc603ad324b634
Mohammed Arabiat <tanjoodo@hotmail.com>
2012-02-11 21:27:29 +0200

    Commented the code with some slight modifications.

--- PythaCalc/Classses/Functions.cs
+++ PythaCalc/Classses/Functions.cs
@@ -1,47 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+//-----------------
 namespace PythaCalc
 {
     class Functions
     {
         
+        // This is the function that figures out whether a triangle is right or not based on its side's lengths.
         public static bool findOut(double num1, double num2, double num3)
         {
+            // doing some serious power stuff.
             double num1p, num2p, num3p;
             num1p = num1 * num1;
             num2p = num2 * num2;
             num3p = num3 * num3;
 
+            // The highest value is the hypotenuse.
             if ((num1 > num2) && (num1 > num3) && (num1p == (num2p + num3p))) return true;
             else if ((num2 > num1) && (num2 > num3) && (num2p == (num1p + num3p))) return true;
             else if ((num3 > num2) && (num3 > num1) && (num3p == (num1p + num2p))) return true;
+            // this line isn't accurate, as there's the possibility of two sides having the same value.
             else return false;
             
         }
 
+        // This method finds the length of the hypotenuse based on the length of the other two sides.
         public static double findHypo(double num1, double num2)
         {
-            double num1p, num2p, hypo;
+            // Declaring stuff.
+            double num1p, num2p;
             num1p = num1 * num1;
             num2p = num2 * num2;
-
-            hypo = Math.Sqrt(num1p + num2p);
-
-            return hypo;
-
+            return Math.Sqrt(num1p + num2p);           
         }
+
+        // This method finds the length of a side if the other two lengths are known.
         public static double findSide(double num1, double num2)
         {
+            // More declaring action over here.
             double num1p, num2p;
-
+            // Setting up the squared values. If you were wondering, the "p" is for "power".
             num1p = num1 * num1;
             num2p = num2 * num2;
 
+            // This is self explanatory.
             if (num1p > num2p) return Math.Sqrt(num1p - num2p);
             else if (num1p < num2p) return Math.Sqrt(num2p - num1p);
+            // I went with -1 because double aren't nullable and it's impossible to get a negative value out of this method.
             else return -1;
         }
 
--- PythaCalc/MainWindow.xaml.cs
+++ PythaCalc/MainWindow.xaml.cs
@@ -1,84 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
+//---------------------------------------------------
 namespace PythaCalc
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    // Hello there! If you can read this, then you have enough time on your hands to be looking at some very stupid and simple code.
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            // This part is pretty essential.
             InitializeComponent();
         }
 
+        // This happens when the Find Out button is clicked.
         private void findOutButton_Click(object sender, RoutedEventArgs e)
         {
+            // Just making sure no blanks are left.
             if ((findOutBox1.Text == "") || (findOutBox2.Text == "") || (findOutBox3.Text == ""))
                 MessageBox.Show("Please enter all required fields.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-
+            // We get dirty here and start calculating. By the way, the actual calculations happen in ~/PythaCalc/Classses/Functions.cs
+            // by the way, the 3 s's in "Classses" was a mistake.
             else
             {
+                // Some magic happening over here. Converting string values in the text boxes into doubles and using them as arguments
+                // for the findOut method.
                 if (Functions.findOut(Convert.ToDouble(findOutBox1.Text),
                     Convert.ToDouble(findOutBox2.Text),
                     Convert.ToDouble(findOutBox3.Text)))
                 {
-                    MessageBox.Show("The triangle is a right one", "Result", MessageBoxButton.OK, MessageBoxImage.Information);
-                   
+                    // If the findOut method returns "true", this happens.
+                    MessageBox.Show("The triangle is right.", "Result", MessageBoxButton.OK, MessageBoxImage.Information);
+
                 }
+                // If the findOut method returns "false", this happens.
+                else MessageBox.Show("The triangle is not right.", "Result", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
 
+        // This happens when the Find Hypotenuse button is clicked.
         private void hypoButton_Click(object sender, RoutedEventArgs e)
         {
+            // Making sure no blanks are left.
             if ((hypoBox1.Text == "") || (hypoBox2.Text == "")) MessageBox.Show("Please enter all required fields.", "Error",
                                                                                 MessageBoxButton.OK, MessageBoxImage.Error);
+            // We start converting textbox strings to doubles and using them as arguments.
             else
             {
+                // There's no error handling here yet. But I should put some. As the application will crash if you put in a letter instead of a character.
                 MessageBox.Show("Result is: " + Convert.ToString(Functions.findHypo(Convert.ToDouble(hypoBox1.Text), Convert.ToDouble(hypoBox2.Text))),
                     "Result", MessageBoxButton.OK, MessageBoxImage.Information);                 
             }
         }
 
+        // This happens when the Find Side button is clicked.
         private void sideButton_Click(object sender, RoutedEventArgs e)
         {
-
+            // Making sure no blanks are left.
             if ((sideBox1.Text == "") || (sideBox2.Text == "")) MessageBox.Show("Please enter all required fields.", "Error",
                                                                                 MessageBoxButton.OK, MessageBoxImage.Error);
+            // Getting dirty with the converting and arguing.
             else
             {
+                // This happens when two sides are the same. I don't know if two sides can have the same length in a right triangle
+                // so I don't know what to do exactly. I used a negative value because it's impossible to have a length with a negative value.
                 if (Functions.findSide(Convert.ToDouble(sideBox1.Text), Convert.ToDouble(sideBox2.Text)) < 0)
                 {
                     MessageBox.Show("There's something wrong, it will probably be fixed soon.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
+
+                // This is obvious.
                 else
                 {
                     MessageBox.Show("Result is: " + Convert.ToString(Functions.findSide(Convert.ToDouble(sideBox1.Text), Convert.ToDouble(sideBox2.Text))), "Result",
                                      MessageBoxButton.OK, MessageBoxImage.Information);
                 }
             }
-                
-
-        }
-
-     
-
-       
-
-     
-
-        
+        }       
     }
 }
